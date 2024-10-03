53b200c8fe42ecf75e651771f85a6a201f06a175
Ilya <ilyaratush@gmail.com>
2024-10-04 01:57:31 +0500

    Rewrote markup in components

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/app/app';
+
+const rentalOffersCount = 312;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -7,6 +10,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <App offersCount={rentalOffersCount} />
   </React.StrictMode>
 );
