702c4dea788de0592fe65d89416749412d5190fa
Ilya <ilyaratush@gmail.com>
2024-10-17 18:21:34 +0500

    Сonfigure routing

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,11 @@
 import MainPage from '../../pages/main-page/main-page';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import LoginPage from '../../pages/login-page/login-page.tsx';
+import FavoritesPage from '../../pages/favorites-page/favorites-page.tsx';
+import OfferPage from '../../pages/offer-page/offer-page.tsx';
+import NotFoundPage from '../../pages/not-found-page/not-found-page.tsx';
+import {AppRoute, AuthorizationStatus} from '../../const.ts';
+import {PrivateRoute} from '../private-route/private-route.tsx';
 
 type AppProps = {
   offersCount: number;
@@ -6,7 +13,36 @@ type AppProps = {
 
 function App({ offersCount }: AppProps): JSX.Element {
   return (
-    <MainPage offersCount={offersCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.MainPage}
+          element={<MainPage offersCount={offersCount} />}
+        />
+        <Route
+          path={AppRoute.LoginPage}
+          element={<LoginPage />}
+        />
+        <Route
+          path={AppRoute.FavoritesPage}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <FavoritesPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.OfferPage}
+          element={<OfferPage />}
+        />
+        <Route
+          path={AppRoute.NotFoundPage}
+          element={<NotFoundPage />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
