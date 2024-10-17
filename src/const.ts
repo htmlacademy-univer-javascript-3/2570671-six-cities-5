bd8cd652ea5d4650ca8ee00a01e12e7c936b0a50
Ilya <ilyaratush@gmail.com>
2024-10-17 18:21:34 +0500

    Сonfigure routing

---
+++ src/const.ts
@@ -0,0 +1,13 @@
+export enum AppRoute{
+  MainPage = '/',
+  LoginPage = '/login',
+  FavoritesPage = '/favorites',
+  OfferPage = '/offer/:id',
+  NotFoundPage = '*',
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
