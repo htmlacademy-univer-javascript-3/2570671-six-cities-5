1d0aa4289e07d5d791dbe5c47fe6a08adf103ae0
Ilya <ilyaratush@gmail.com>
2024-12-20 16:23:21 +0500

    и еще немного порефакторил код

--- src/components/reviews-list/reviews-list.tsx
+++ src/components/reviews-list/reviews-list.tsx
@@ -8,20 +8,17 @@ type ReviewsListProps = {
 
 function ReviewsList({reviews}: ReviewsListProps): JSX.Element {
   return (
-    <section className="offer__reviews reviews">
-      <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviews.length}</span></h2>
-      <ul className="reviews__list">
-        {
-          reviews
-            .map((review) => (
-              <MemoizedReviewItem
-                key={review.id}
-                review={review}
-              />)
-            )
-        }
-      </ul>
-    </section>
+    <ul className="reviews__list">
+      {
+        reviews
+          .map((review) => (
+            <MemoizedReviewItem
+              key={review.id}
+              review={review}
+            />)
+          )
+      }
+    </ul>
   );
 }
 
